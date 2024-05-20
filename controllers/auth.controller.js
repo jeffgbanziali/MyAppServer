f5b1977d78e4f6e22159835d689a4da2ecfb1588
Jeff GBANZIALI <jeffgbanziali@MacBook-Pro-de-Jeff.local>
2024-05-20 02:41:07 +0200

    uopdate

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,9 +21,9 @@ const transporter = nodemailer.createTransport({
     auth: {
         type: 'OAuth2',
         user: process.env.EMAIL_USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
+   //     clientId: process.env.CLIENT_ID,
+       // clientSecret: process.env.CLIENT_SECRET,
+        //refreshToken: process.env.REFRESH_TOKEN,
         //accessToken: 'your-access-token' // facultatif, mais peut être utilisé
     }
 });
